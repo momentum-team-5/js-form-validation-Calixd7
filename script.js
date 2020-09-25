2d59dde3e549de5468fc692b14d62887dc38cd17
Calixd7 <calixd7@gmail.com>
2020-09-25 15:21:34 -0400

    first push

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('Add validation!');
+let parkingform = document.querySelector('#parkingform')
+
+parkingform.addEventListener('submit', validateForm)
+
+
+fetch('https://momentum-server.glitch.me/parking', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ formData: { name: ''}})
+    })
+    .then(res => res.json())
+    .then(res => { console.log(res)
+       
+    })
+    function validateCardNumber(number) {
+        var regex = new RegExp("^[0-9]{16}$");
+        if (!regex.test(number))
+            return false;
+    
+        return luhnCheck(number);
+    }
+    
+    function luhnCheck(val) {
+        var sum = 0;
+        for (var i = 0; i < val.length; i++) {
+            var intVal = parseInt(val.substr(i, 1));
+            if (i % 2 == 0) {
+                intVal *= 2;
+                if (intVal > 9) {
+                    intVal = 1 + (intVal % 10);
+                }
+            }
+            sum += intVal;
+        }
+        return (sum % 10) == 0;
+    }
+    ```
